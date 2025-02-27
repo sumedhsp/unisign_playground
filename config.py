a82a199403f6d8c276ae3c9a7417ec1fab39b78d
Sumedh Parvatikar <sumedhsparvatikar@gmail.com>
2025-02-26 20:46:55 -0500

    Changed the path for WLASL metadata

--- config.py
+++ config.py
@@ -4,19 +4,19 @@ mt5_path = "./pretrained_weight/mt5-base"
 train_label_paths = {
                     "CSL_News": "./data/CSL_News/CSL_News_Labels.json",
                     "CSL_Daily": "./data/CSL_Daily/labels.train",
-                    "WLASL": "./data/WLASL/labels-2000.train"
+                    "WLASL": "unisign_playground/data/WLASL/labels-2000.train"
                     }
 
 dev_label_paths = {
                     "CSL_News": "./data/CSL_News/CSL_News_Labels.json",
                     "CSL_Daily": "./data/CSL_Daily/labels.dev",
-                    "WLASL": "./data/WLASL/labels-2000.dev"
+                    "WLASL": "unisign_playground/data/WLASL/labels-2000.dev"
                     }
 
 test_label_paths = {
                     "CSL_News": "./data/CSL_News/CSL_News_Labels.json",
                     "CSL_Daily": "./data/CSL_Daily/labels.test",
-                    "WLASL": "./data/WLASL/labels-2000.test"
+                    "WLASL": "unisign_playground/data/WLASL/labels-2000.test"
                     }
 
 
