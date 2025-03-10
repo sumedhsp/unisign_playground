b7e2de0cad2cb9d96d0a17edcd92b4863e09baf1
Sumedh Parvatikar <sumedhsparvatikar@gmail.com>
2025-03-09 22:33:22 -0400

    Updating the config to reflect to the new metadata

--- config.py
+++ config.py
@@ -5,19 +5,19 @@ mt5_repo = "google/mt5-base"
 train_label_paths = {
                     "CSL_News": "./data/CSL_News/CSL_News_Labels.json",
                     "CSL_Daily": "./data/CSL_Daily/labels.train",
-                    "WLASL": "unisign_playground/data/WLASL/labels-2000.train"
+                    "WLASL": "unisign_playground/data/WLASL/filtered_labels-2000.train"
                     }
 
 dev_label_paths = {
                     "CSL_News": "./data/CSL_News/CSL_News_Labels.json",
                     "CSL_Daily": "./data/CSL_Daily/labels.dev",
-                    "WLASL": "unisign_playground/data/WLASL/labels-2000.dev"
+                    "WLASL": "unisign_playground/data/WLASL/filtered_labels-2000.dev"
                     }
 
 test_label_paths = {
                     "CSL_News": "./data/CSL_News/CSL_News_Labels.json",
                     "CSL_Daily": "./data/CSL_Daily/labels.test",
-                    "WLASL": "unisign_playground/data/WLASL/labels-2000.test"
+                    "WLASL": "unisign_playground/data/WLASL/filtered_labels-2000.test"
                     }
 
 
